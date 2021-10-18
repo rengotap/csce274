00b2dfc7e10902241996b11d575699804e0199f6
Renato Gonzalez-Tapia <RENATOG@email.sc.edu>
2021-10-18 18:01:52 -0400

    temp

--- packages/project2/src/circle.py
+++ packages/project2/src/circle.py
@@ -4,12 +4,12 @@ import rospy
 from duckietown_msgs.msg import Twist2DStamped
 
 def circle():
-    circpub = rospy.Publisher('duck16/car_cmd_switch_node/cmd',Twist2DStamped, queue_size=1)  # may not be publishing correctly
+    circpub = rospy.Publisher('duck16/car_cmd_switch_node/cmd',Twist2DStamped, queue_size=10)  # may not be publishing correctly
     rospy.init_node('circle', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
 
     while not rospy.is_shutdown():
-        msg = Twist2DStamped(header=None, v=1.0, omega=0)
+        msg = Twist2DStamped(header=None, v=1, omega=1)
 
         circpub.publish(msg)
         rate.sleep()
