9ee5b02a5da72d3d5fa6f1e6835765ca5d272e04
Renato Gonzalez-Tapia <RENATOG@email.sc.edu>
2021-10-18 18:44:05 -0400

    temp

--- packages/project2/src/circle.py
+++ packages/project2/src/circle.py
@@ -6,7 +6,7 @@ from duckietown_msgs.msg import Twist2DStamped
 def circle():
     circpub = rospy.Publisher('duck16/car_cmd_switch_node/cmd',Twist2DStamped, queue_size=10)  # may not be publishing correctly?
     rospy.init_node('circle')
-    rate = rospy.Rate(10)  # 10hz
+    rate = rospy.Rate(1)  # 10hz
     msg = Twist2DStamped(v=0.5, omega=0)
 
     circpub.publish(msg)
