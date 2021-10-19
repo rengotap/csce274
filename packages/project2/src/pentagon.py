8f1ba40a9e4f09a38209a7e8ff878d4cbb44faa5
Renato Gonzalez-Tapia <RENATOG@email.sc.edu>
2021-10-19 13:15:24 -0400

    temp

--- packages/project2/src/pentagon.py
+++ packages/project2/src/pentagon.py
@@ -6,7 +6,7 @@ from duckietown_msgs.msg import Twist2DStamped
 
 def pentagon():
     pentapub = rospy.Publisher('/duck16/car_cmd_switch_node/cmd',Twist2DStamped, queue_size=10)
-    rospy.init_node('pentagon', anonymous=True)
+    rospy.init_node('pentagon')
     rate = rospy.Rate(10)  # 10hz
 
     for i in range(5):  # loop 5x b/c pentagon
