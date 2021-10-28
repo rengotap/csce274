697d857a424bb462fa24a284d0066ea79912d6de
Renato Gonzalez-Tapia <RENATOG@email.sc.edu>
2021-10-28 11:34:33 -0400

    f

--- packages/homework5/src/converter.py
+++ packages/homework5/src/converter.py
@@ -6,21 +6,21 @@ from std_msgs.msg import Float32
 class Converter:
     def __init__(self):  # 
         rospy.Subscriber("/homework2/total", Float32, self.callback)  # listening to hw2 total
-        self.pub = rospy.Publisher("/homework3/hw3sub", Float32, queue_size=10)  # publishing to hw3 subscriber
+        self.pub = rospy.Publisher("/homework3/hw3pub", Float32, queue_size=10)  # publishing to hw3 subscriber
         self.output = 0
 
     def callback(self, data):
         # convert to meters
-        self.output = data/3.218
+        self.output = data/float(3.218)
         self.pub.publish(self.output)
         # convert to feet
         self.output = data
         self.pub.publish(self.output)
         # convert to smoot
-        self.output = data/5.583
+        self.output = data/float(5.583)
         self.pub.publish(self.output)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     rospy.init_node('converter')
     Converter()
     rospy.spin()
